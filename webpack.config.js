0b18753208ae0543c0a388ae353a4c360fdf1d82
elmatou <ma@brenac.org>
2021-10-04 15:03:07 +0200

    setup stack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,38 @@
+const { VueLoaderPlugin } = require('vue-loader');
+
 module.exports = {
-    output: {
-        path: __dirname,
-        filename: 'dist/main.js',
-        library: 'vue-mapx',
-    },
-}; 
+  mode: 'none',
+  output: {
+    path: __dirname,
+    publicPath: '',
+    filename: 'dist/main.js',
+    library: { name: 'VueMapX', type: 'var' },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+  ],
+  devServer: {
+    static: ['demo'],
+    compress: true,
+    port: 9000,
+    open: true,
+  },
+  externals: {
+    vue: 'Vue',
+  },
+};
